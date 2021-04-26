da0733fc54091f3190e376393bdd325d5498472f
Alex Lin <alexlin247@gmail.com>
2021-04-25 20:23:13 -0400

    Edit styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,9 @@ const Home: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="text-4xl">Welcome to the Brain.</h1>
+        <div className="flex justify-center items-center">
+          <h1 className="text-4xl">Welcome to the Brain.</h1>
+        </div>
       </main>
     </div>
   );
