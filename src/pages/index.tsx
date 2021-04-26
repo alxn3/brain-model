b9c24ebb7b9d16b30b4a5291a2edf36bd9acb621
Alex Lin <alexlin247@gmail.com>
2021-04-25 20:16:23 -0400

    Create src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Welcome to the Brain.</h1>
+        <h1 className="text-4xl">Welcome to the Brain.</h1>
       </main>
     </div>
   );
