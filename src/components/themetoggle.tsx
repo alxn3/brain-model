8eba78812812425415126e4c287fce544e929246
Alex Lin <alexlin247@gmail.com>
2021-04-27 14:44:22 -0400

    Fix initial theme error

--- src/components/themetoggle.tsx
+++ src/components/themetoggle.tsx
@@ -5,15 +5,27 @@ type Props = {
 };
 
 const ThemeToggle: React.FC<Props> = ({ className }) => {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
 
   return (
     <button
       className={
-        'rounded-md p-2 font-mono text-black bg-gray-200 dark:text-white dark:bg-gray-900 hover:bg-gray-300 dark:hover:bg-gray-800 transition-colors ' +
+        'rounded-md p-2 font-mono focus:outline-none text-black bg-gray-200 dark:text-white dark:bg-gray-900 hover:bg-gray-300 dark:hover:bg-gray-800 transition-colors ' +
         className
       }
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => {
+        switch (theme) {
+          case 'light':
+            setTheme('dark');
+            break;
+          case 'dark':
+            setTheme('light');
+            break;
+          default:
+            setTheme(systemTheme === 'light' ? 'dark' : 'light');
+            break;
+        }
+      }}
     >
       Toggle Theme
     </button>
